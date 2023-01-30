32fe7c194cac7a8cb1f975bbe336f380b117be5f
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-31 01:15:46 +0530

    Update sk.php

--- modules/sk.php
+++ modules/sk.php
@@ -33,20 +33,20 @@ $pending = trim(strip_tags(getStr($result1,'"livemode": true,
   $time_end = microtime(true);
   $execution_time = ($time_end - $time_start);
 if (strpos($result, 'api_key_expired')){
-sendMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: DEAD KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
+sendMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: DEAD KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
 }elseif (strpos($result, 'Invalid API Key provided')){
-sendMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: DEAD KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
+sendMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: DEAD KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
 }
 elseif ((strpos($result, 'You did not provide an API key.')) || (strpos($result, 'You need to provide your API key in the Authorization header,'))){
-sendMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: MISSING KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
+sendMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: MISSING KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
 }
 elseif ((strpos($result, 'rate_limit')) || (strpos($result, 'rate_limit'))){
-sendMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: VALID SK KEY✅%0A[ϟ] RESPONSE: RATE LIMIT ⚠️.%0A[ϟ] CURRENCY: $curr%0A[ϟ] BALANCE: $balance%0A[ϟ] PENDING AMOUNT: $pending%0A[ϟ] CARDS PROCESSED: $pending%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
+sendMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: VALID SK KEY✅%0A[ϟ] RESPONSE: RATE LIMIT ⚠️.%0A[ϟ] CURRENCY: $curr%0A[ϟ] BALANCE: $balance%0A[ϟ] PENDING AMOUNT: $pending%0A[ϟ] CARDS PROCESSED: $pending%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
 }  
 elseif ((strpos($result, 'testmode_charges_only')) || (strpos($result, 'test_mode_live_card'))){
-sendMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: DEAD KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>", $message_id);
+sendMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: DEAD KEY❌%0A[ϟ] RESPONSE: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>", $message_id);
 }else{
-sendMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: VALID SK KEY✅%0A[ϟ] RESPONSE: Provided SK KEY is Live!✅%0A[ϟ] CURRENCY: $curr%0A[ϟ] BALANCE: $balance%0A[ϟ] PENDING AMOUNT: $pending%0A[ϟ] CARDS PROCESSED: $pending%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
+sendMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: SK KEY CHK%0A[ϟ] SK KEY: <code>$sec</code>%0A[ϟ] STATUS: VALID SK KEY✅%0A[ϟ] RESPONSE: Provided SK KEY is Live!✅%0A[ϟ] CURRENCY: $curr%0A[ϟ] BALANCE: $balance%0A[ϟ] PENDING AMOUNT: $pending%0A[ϟ] CARDS PROCESSED: $pending%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>%0A", $message_id);
 }
 }
 ?>
