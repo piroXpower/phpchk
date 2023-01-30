0f78b6d876328ab703418523d366f1d213250620
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-31 01:17:47 +0530

    Update iban.php

--- modules/iban.php
+++ modules/iban.php
@@ -25,6 +25,6 @@ $msg = trim(strip_tags(getStr($result2,'{
   $time_end = microtime(true);
   $execution_time = ($time_end - $time_start);
   
-sendMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] TOOL: IBAN CHK%0A[ϟ] IBAN: <code>$iban</code>%0A[ϟ] STATUS: $ib%0A[ϟ] MSG: $msg%0A[ϟ] BANK: $bankname%0A[ϟ]BANK CODE: $bankcode1%0A[ϟ]BIC: $bic[ϟ]CITY: $city%0A[ϟ]ZIP CODE: $zip%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>", $message_id);
+sendMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] TOOL: IBAN CHK%0A[ϟ] IBAN: <code>$iban</code>%0A[ϟ] STATUS: $ib%0A[ϟ] MSG: $msg%0A[ϟ] BANK: $bankname%0A[ϟ]BANK CODE: $bankcode1%0A[ϟ]BIC: $bic[ϟ]CITY: $city%0A[ϟ]ZIP CODE: $zip%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>", $message_id);
 }
 
