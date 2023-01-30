86b85eecf60427df34b0f46b0d4527607dbb30d6
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-31 01:17:17 +0530

    Update ip.php

--- modules/ip.php
+++ modules/ip.php
@@ -4,7 +4,7 @@ if ((strpos($message, "/ip") === 0)||(strpos($message, "!ip") === 0)||(strpos($m
   sendaction($chatId, typing);
 $ip = substr($message, 4);
 
-    $sendmes = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=".$chatId."&text=<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] IP: ".$ip."%0A[ϟ] STATUS: CHECKING IP,PLEASE WAIT%0A━━━━━━━━━━━━━</b>&reply_to_message_id=".$message_id."&parse_mode=HTML";
+    $sendmes = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=".$chatId."&text=<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] IP: ".$ip."%0A[ϟ] STATUS: CHECKING IP,PLEASE WAIT%0A━━━━━━━━━━━━━</b>&reply_to_message_id=".$message_id."&parse_mode=HTML";
   
     $sent = json_decode(file_get_contents($sendmes) ,1);
       $mes_id = $sent['result']['message_id'];
@@ -42,6 +42,6 @@ $pcode = trim(strip_tags(getStr($gip,'  <tr>
   $time_end = microtime(true);
   $execution_time = ($time_end - $time_start);
   
-editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] TOOL: IP CHK%0A[ϟ] IP: $ip%0A[ϟ] RAW DETAILS: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] ISP: $isp%0A[ϟ] ASN: $asn%0A[ϟ] COUNTRY: $country - $countrycode%0A[ϟ] Region: $region%0A[ϟ] CITY: $city%0A[ϟ] POSTAL CODE: $pcode%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>", $mes_id);
+editMessage($chatId, "<b>BLAZE CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] TOOL: IP CHK%0A[ϟ] IP: $ip%0A[ϟ] RAW DETAILS: <code>$msg</code>%0A━━━━━━━━━━━━━%0A[ϟ] ISP: $isp%0A[ϟ] ASN: $asn%0A[ϟ] COUNTRY: $country - $countrycode%0A[ϟ] Region: $region%0A[ϟ] CITY: $city%0A[ϟ] POSTAL CODE: $pcode%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>", $mes_id);
 }
 
